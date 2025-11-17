747cf15014038085dd130c3b344758ec3d5a7f6f
Hironmoy Sikder <hironmoyhsd.1789@gmail.com>
2025-11-17 22:20:17 +0600

    Task gets deleted when complete.

--- web.py
+++ web.py
@@ -8,13 +8,17 @@ def add_task():
     tasks.append(task)
     functions.write_tasks(tasks)
 
-
 st.title('Worknote')
 st.subheader('Your Efficiency Solution')
 st.write('Your tasks')
 
-for task in tasks:
-    st.checkbox(task)
+for index, task in enumerate(tasks):
+    checkbox = st.checkbox(task, key=task)
+    if checkbox:
+        tasks.pop(index)
+        functions.write_tasks(tasks)
+        del st.session_state[task]
+        st.rerun()
 
 st.text_input(label='', placeholder='Enter a task',
               on_change=add_task, key='new_task')
